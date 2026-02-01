f2e508d03cb75ebab1ef5d0615bf9699f07b0f53
aminghoreishi <amin.ghoreishi86@gmail.com>
2026-02-01 11:22:23 +0330

    feat: integrate Cloudinary for image uploads in brand API and enhance POST request handling

--- src/app/api/brand/route.ts
+++ src/app/api/brand/route.ts
@@ -1,7 +1,14 @@
 import brandModel from "@/models/brand";
 import db from "@/config/db";
 import { NextRequest, NextResponse } from "next/server";
-import { authRouteHandler, verifyAccessToken } from "@/app/utils/auth";
+import { authRouteHandler } from "@/app/utils/auth";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
+});
 
 export async function POST(req: NextRequest) {
   const isAdmin = authRouteHandler(req.headers.get("Authorization"));
@@ -10,7 +17,39 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ message: "Access denied" }, { status: 403 });
   }
 
-  return NextResponse.json({ message: "ok" });
+  const formData = await req.formData();
+  const title = formData.get("title") as string;
+  const file = formData.get("image") as File;
+
+  if (!title || !file) {
+    return NextResponse.json(
+      { message: "Title و Image الزامی هستند" },
+      { status: 400 },
+    );
+  }
+
+  const buffer = Buffer.from(await file.arrayBuffer());
+
+  const uploadResult = await new Promise<any>((resolve, reject) => {
+    cloudinary.uploader
+      .upload_stream(
+        {
+          folder: "karin/brand",
+        },
+        (error, result) => {
+          if (error) reject(error);
+          else resolve(result);
+        },
+      )
+      .end(buffer);
+  });
+
+  const brand = await brandModel.create({
+    title,
+    imageUrl: uploadResult.secure_url,
+  });
+
+  return NextResponse.json(brand, { status: 201 });
 }
 
 export async function GET() {
