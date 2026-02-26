a757cfeaa86152b290dc9cc2e5844cfa3caea998
aminghoreishi <amin.ghoreishi86@gmail.com>
2026-02-26 12:50:53 +0330

    feat: enhance Footer component with improved styling and layout consistency

--- src/components/module/Footer/Footer.tsx
+++ src/components/module/Footer/Footer.tsx
@@ -4,19 +4,20 @@ import FooterTopBtn from "./FooterTopBtn";
 export default function Footer() {
   return (
     <div className="bg-slate-900 dark:bg-slate-800 rounded-xl p-5 mb-12">
-      <div className="grid grid-cols-1  md:grid-cols-3 lg:grid-cols-4 gap-5">
-        <div>
-          <h2 className="text-white">درباره کارین شاپ</h2>
-          <p className="text-gray-300 mt-5 text-sm">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        <div className="lg:col-span-2">
+          <h2 className="text-white font-bold">درباره کارین شاپ</h2>
+          <p className="text-gray-400 mt-5 leading-8">
             در فروشگاه آنلاین ما، بهترین مدل‌های موبایل و لپ‌تاپ از برندهای
             معتبر جهانی را با کیفیت بالا و قیمتی مناسب برای شما فراهم آورده‌ایم.
             با انتخاب محصولات ما، تجربه‌ای حرفه‌ای و لذت‌بخش از فناوری را در
             خانه یا محل کار داشته باشید.
           </p>
         </div>
+
         <div>
-          <h2 className="text-white">دسترسی سریع</h2>
-          <ul className="text-gray-300 flex flex-col gap-5 text-sm mt-5">
+          <h2 className="text-white font-bold">دسترسی سریع</h2>
+          <ul className="text-gray-400 flex flex-col gap-5 text-sm mt-5">
             <li>صحفه اصلی</li>
             <li>فروشگاه</li>
             <li>تماس با ما</li>
@@ -24,8 +25,8 @@ export default function Footer() {
           </ul>
         </div>
         <div>
-          <h2 className="text-white">تماس با ما</h2>
-          <ul className="text-gray-300 mt-5 text-sm">
+          <h2 className="text-white font-bold">تماس با ما</h2>
+          <ul className="text-gray-400 mt-5 text-sm">
             <li className="flex items-center justify-between">
               <p>شماره تماس:</p>
               <p>۰۹۰۵۲۰۱۸۷۵۱</p>
--- src/components/template/ProductInfo/ContainerFeCoIN/Intro/Intro.tsx
+++ src/components/template/ProductInfo/ContainerFeCoIN/Intro/Intro.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Intro({ description }: { description: string }) {
-  return <div>{description}</div>;
+  return <div dangerouslySetInnerHTML={{ __html: description }}></div>;
 }
